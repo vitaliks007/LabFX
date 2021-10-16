c82e78fd185cf2802a1ebbff64a0a3eb47516d6f
vitaliks007 <45079744+vitaliks007@users.noreply.github.com>
2021-10-16 10:01:53 +0300

    Lab11_12

--- src/main/java/com/mireahw/lab11_12/Controller.java
+++ src/main/java/com/mireahw/lab11_12/Controller.java
@@ -4,14 +4,38 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    private int result = 0;
+    private double result = 0;
 
     @FXML
     private TextField textFieldNumber;
 
     @FXML
     public void buttonPlusAction() {
-        result += textFieldNumber;
+        result += Double.parseDouble(textFieldNumber.getText());
+        textFieldNumber.clear();
+    }
+
+    @FXML
+    public void buttonMinusAction() {
+        result -= Double.parseDouble(textFieldNumber.getText());
+        textFieldNumber.clear();
+    }
+
+    @FXML
+    public void buttonMultipleAction() {
+        result *= Double.parseDouble(textFieldNumber.getText());
+        textFieldNumber.clear();
+    }
+
+    @FXML
+    public void buttonDivideAction() {
+        result /= Double.parseDouble(textFieldNumber.getText());
+        textFieldNumber.clear();
+    }
+
+    @FXML
+    public void buttonCalculateAction() {
+        textFieldNumber.setText(String.valueOf(result));
     }
 
 }
